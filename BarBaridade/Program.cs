b0fd3f08133fa6eb96f98475763b4df7da7c68f7
José Farah <zefarah2@gmail.com>
2025-05-15 23:43:13 -0300

    adicionando mapPut/mapDelete, HTML inicial puro e JS

--- BarBaridade/Program.cs
+++ BarBaridade/Program.cs
@@ -5,7 +5,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=bebidas.db"));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors((options) => 
+{
+    options.AddDefaultPolicy((policy) => 
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors();
+
+app.UseHttpsRedirection();
 
 app.MapGet("/bebidas", (AppDbContext db) => {
     var bebidas = db.TabelaBebidas.ToList();
@@ -13,14 +30,37 @@ app.MapGet("/bebidas", (AppDbContext db) => {
 });
 
 app.MapGet("/bebidas/{id}", async (int id, AppDbContext db) => {
-    var bebidas = await db.TabelaBebidas.FindAsync(id);
-    return bebidas is not null ? Results.Ok(bebidas) : Results.NotFound("Bebida não encontrada!");
+    var bebida = await db.TabelaBebidas.FindAsync(id);
+    return bebida is not null ? Results.Ok(bebida) : Results.NotFound("Bebida não encontrada!");
 });
 
 app.MapPost("/bebidas", async (Bebida bebida, AppDbContext db) => {
     db.TabelaBebidas.Add(bebida);
     await db.SaveChangesAsync();
     return Results.Created($"/bebidas/{bebida.Id}", bebida);
+});
+
+app.MapPut("/bebidas/{id}", async (int id, Bebida updatedBebida, AppDbContext db) => {
+    var existing = await db.TabelaBebidas.FindAsync(id);
+    if (existing == null){
+        return Results.NotFound("Bebida não encontrada!");
+    }
+    existing.Nome = updatedBebida.Nome;
+    existing.TeorAlcoolico = updatedBebida.TeorAlcoolico;
+
+    await db.SaveChangesAsync();
+    return Results.Ok(existing);
+});
+
+app.MapDelete("/bebidas/{id}", async (int id, AppDbContext db) => {
+    var bebida = await db.TabelaBebidas.FindAsync(id);
+    if (bebida == null){
+        return Results.NotFound("Bebida não encontrada!");
+    }
+
+    db.TabelaBebidas.Remove(bebida);
+    await db.SaveChangesAsync();
+    return Results.Ok("Bebida retirada do catálogo com sucesso!");
 });
 
 app.Run();
---
+++ BarBaridade/script.js
@@ -0,0 +1,176 @@
+const listaBebidas = document.getElementById('lista-bebidas');
+const btnGetTodas = document.getElementById('btn-todas');
+const inputId = document.getElementById('input-buscaId');
+const formId = document.getElementById('form-buscaId');
+const inputNome = document.getElementById('newNome');
+const inputTeor = document.getElementById('newTeor');
+const formPost = document.getElementById('form-post');
+const formPut = document.getElementById('form-put');
+const formDelete = document.getElementById('form-delete');
+const apiURL = 'http://localhost:5094/bebidas';
+
+const getBebidas = async () => {
+    listaBebidas.innerHTML = '';
+
+    try {
+        const response = await fetch(apiURL, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        
+        if(!response.ok){
+            throw new Error('Erro ao buscar as bebidas.');
+        }
+
+        const bebida = await response.json();
+
+        bebida.forEach(bebida => {
+            const newLi = document.createElement('li');
+            newLi.innerHTML = `ID: ${bebida.id} | Nome: ${bebida.nome} | Teor Alcoólico: ${bebida.teorAlcoolico}%`;
+            listaBebidas.appendChild(newLi);
+        });
+
+    } 
+    catch (error) {
+        console.log(error.message);
+        listaBebidas.innerText = `${error.message}`;
+    }
+}
+
+const getBebidaPorId = async (id) => {
+    listaBebidas.innerHTML = '';
+
+    try {
+        const response = await fetch(`${apiURL}/${id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok){
+            throw new Error("Bebida não encontrada!");
+        }
+
+        const bebida = await response.json();
+
+        const newLi = document.createElement('li');
+        newLi.innerText = `ID: ${bebida.id} | Nome: ${bebida.nome} | Teor Alcoólico: ${bebida.teorAlcoolico}%`;
+        listaBebidas.appendChild(newLi);
+    } catch (error) {
+        console.log(error.message);
+        listaBebidas.innerText = `${error.message}`;
+        alert(error.message);
+    }
+}
+
+const postBebida = async (novaBebida) => {
+    listaBebidas.innerHTML = '';
+    
+    try {
+        const response = await fetch(apiURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(novaBebida)
+        });
+
+        if (!response.ok) {
+            throw new Error("Erro ao adicionar nova bebida!");         
+        }
+
+        const bebida = await response.json();
+
+        alert(`${bebida.nome} foi adicionado(a) ao catálogo!`);
+    } catch (error) {
+        console.log(error.message);
+        alert(error.message);
+    }
+}
+
+const putBebida = async () => {
+    const id = document.getElementById('updatedId').value;
+    const nome = document.getElementById('updatedNome').value;
+    const teorAlcoolico = document.getElementById('updatedTeor').value;
+    
+    listaBebidas.innerHTML = '';
+    
+    try {
+        const response = await fetch(`${apiURL}/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                nome, 
+                teorAlcoolico
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error("Erro ao editar bebida!");         
+        }
+
+        const bebida = await response.json();
+
+        alert(`A bebida ${bebida.nome} foi atualizado(a) com sucesso!`);
+    } catch (error) {
+        console.log(error.message);
+        alert(error.message);
+    }
+}
+
+const deleteBebida = async () => {
+    const id = document.getElementById('input-deleteId').value;
+    listaBebidas.innerHTML = '';
+    
+    try {
+        const response = await fetch(`${apiURL}/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error("Erro ao apagar bebida!");         
+        }
+
+        // const resultado = await response.text();
+
+        alert(`A bebida foi retirada do catálogo!`);
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+btnGetTodas.addEventListener('click', (event) => {
+    event.preventDefault();
+    getBebidas();
+});
+
+formId.addEventListener('submit', (event) => {
+    event.preventDefault();
+    getBebidaPorId(inputId.value);
+});
+
+formPost.addEventListener('submit', (event) => {
+    event.preventDefault();
+    postBebida({
+        nome: inputNome.value,
+        peso: inputTeor.value
+    });
+});
+
+formPut.addEventListener('submit', (event) => {
+    event.preventDefault();
+    putBebida();
+});
+
+formDelete.addEventListener('submit', (event) => {
+    event.preventDefault();
+    deleteBebida();
+});
